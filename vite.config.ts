4de667b0ca8685db36135543df149be8a374e1a2
deaamelias <deamelyaa07@gmail.com>
2025-08-20 16:18:49 +0700

    add login ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// ESM-compatible __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+});
