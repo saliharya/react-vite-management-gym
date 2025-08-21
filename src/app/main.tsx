e4ab15b11d81a1f9946b480612551fac27c50ccf
Alfira2100018427 <alfira2100018427@webmail.uad.ac.id>
2025-08-21 22:06:53 +0700

    Add registration functionality with form and routing

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,12 +3,14 @@ import "../styles/index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import LoginPage from "@/pages/login/page.tsx";
+import RegisterPage from "@/pages/register/page.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="login" element={<LoginPage />} />
+      <Route path="register" element={<RegisterPage />} />
     </Routes>
   </BrowserRouter>
 );
