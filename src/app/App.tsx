713bf40f03372142480773383872fbb3cfa7304a
fahrir <fakhrirrahman7@gmail.com>
2025-08-23 14:53:11 +0700

    feat: add new components for features, how it works, services, and sponsors sections
    - Implemented Features component with a list of features and badges.
    - Created HowItWorks component to display a step-by-step guide with icons.
    - Developed Services component showcasing client-centric services with descriptions and icons.
    - Added Sponsors component to list sponsors with icons and names.
    - Introduced Cta component as a placeholder for future call-to-action functionality.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,19 +2,29 @@ import {ThemeProvider} from "@/components/theme-provider.tsx";
 import { Navbar } from "@/components/Navbar.tsx";
 import { About } from "@/components/About";
 import { Hero } from "@/components/Hero.tsx";
+import { Sponsors } from "@/components/Sponsor";
+import { HowItWorks } from "@/components/HowItWorks";
+import { Features } from "@/components/Features";
+import { Services } from "@/components/Service";
+
 
 import "@/app/App.css";
 
 export function IndexPages() {
-  return (
-    <>
-      <ThemeProvider>
+return (
+<>
+    <ThemeProvider>
         <Navbar />
         <Hero />
+        <Sponsors />
         <About />
-      </ThemeProvider>
-    </>
-  );
+        <HowItWorks />
+        <Features />
+        <Services />
+
+    </ThemeProvider>
+</>
+);
 }
 
 export default IndexPages;
